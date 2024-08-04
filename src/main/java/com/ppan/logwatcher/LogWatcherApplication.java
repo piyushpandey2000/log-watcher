ea7abc44f071e66f50d22bbc09f2cc5d7db1199d
piyushpandey2000 <piyushpandeywayne@gmail.com>
2024-08-04 21:49:28 +0530

    initialized repo

---
+++ src/main/java/com/ppan/logwatcher/LogWatcherApplication.java
@@ -0,0 +1,13 @@
+package com.ppan.logwatcher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogWatcherApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LogWatcherApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ppan/logwatcher/LogWatcherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ppan.logwatcher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogWatcherApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
